c5e0c87d3dbad62802f1641c45fe55de276357e6
Minwei Xu <faceswilliam@gmail.com>
2016-12-03 21:43:59 -0500

    linting

--- frontend/js/application/components/searchcard/SearchCard.jsx
+++ frontend/js/application/components/searchcard/SearchCard.jsx
@@ -1,15 +1,12 @@
-'use strict';
-
 import React from 'react';
-import lightTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
-import SearchBar from './SearchBar.jsx';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import SearchBar from './SearchBar';
 
-var request = require('superagent');
-const errorMessage = "Sorry no result found";
+const request = require('superagent');
+
+const errorMessage = 'Sorry no result found';
 
 class SearchCard extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -21,30 +18,20 @@ class SearchCard extends React.Component {
         request
             .get('http://127.0.0.1:3000/university_list')
             .end(function (err, res) {
-                    if (res) {
-                        self.setState({
-                            universities: JSON.parse(res.text)
-                        })
-                    }
-                });
-    }
-    
-    requestUniversity(name, done) {
-        request
-            .get(`http://127.0.0.1:3000/university/${name}`)
-            .end(function (err, res) {
-                    if (res) {
-                        done(err, res)
-                    }
-                });
+                if (res) {
+                    self.setState({
+                        universities: JSON.parse(res.text),
+                    });
+                }
+            });
     }
 
     renderUniversity() {
-        var res = [];
-        if (this.state.university) {
-            var university = this.state.university;
-            if (university == errorMessage) {
-                return <h3 style={{color: 'red'}}>{errorMessage}</h3>;
+        const res = [];
+        const university = this.state.university;
+        if (university) {
+            if (university === errorMessage) {
+                return <h3 style={{ color: 'red' }}>{errorMessage}</h3>;
             }
             var key = 0;
             for (var prop in university) {
@@ -57,50 +44,63 @@ class SearchCard extends React.Component {
         return res;
     }
 
-    _onSearched(university) {
+    onSearched(university) {
         this.setState({
             university: '',
-            expanded: true
+            expanded: true,
         });
         this.req = this.requestUniversity(university, function (err, res) {
             console.log(res.text);
             if (JSON.parse(res.text)) {
                 this.setState({
-                    university: JSON.parse(res.text)
-                })} else {
-                    this.setState({
-                        university: errorMessage
-                    })
-                }
-                
-        }.bind(this)); 
+                    university: JSON.parse(res.text),
+                });
+            } else {
+                this.setState({
+                    university: errorMessage,
+                });
+            }
+        }.bind(this));
     }
 
-    _handleExpandChange(expanded) {
-        this.setState({expanded: expanded});
+    requestUniversity(name, done) {
+        request
+            .get(`http://127.0.0.1:3000/university/${name}`)
+            .end(function (err, res) {
+                if (res) {
+                    done(err, res);
+                }
+            });
+    }
+
+    handleExpandChange(expanded) {
+        this.setState({ expanded });
     }
 
     render() {
         return (
-            <Card expanded={ this.state.expanded } onExpandChange={ this._handleExpandChange.bind(this) }
-                style={{ marginTop: '10px', marginBottom: '10px' }}>
-                <CardHeader showExpandableButton title="Search a university" />
+          <Card
+            expanded={this.state.expanded}
+            onExpandChange={ this.handleExpandChange.bind(this)}
+            style={{ marginTop: '10px', marginBottom: '10px' }}
+          >
+            <CardHeader showExpandableButton title="Search a university" />
 
-                <SearchBar onSearched={ this._onSearched.bind(this) }
-                    universities={ this.state.universities } 
-                    hintText="Which university are you interested in?"
-                />
+            <SearchBar
+              onSearched={this.onSearched.bind(this)}
+              universities={this.state.universities}
+              hintText="Which university are you interested in?"
+            />
 
-                <CardText expandable={true}>
-                    { this.renderUniversity() }
-                </CardText>
-            </Card>
-        );
+            <CardText expandable>
+              { this.renderUniversity() }
+            </CardText>
+          </Card>);
     }
 }
 
 SearchCard.childContextTypes = {
-    muiTheme: React.PropTypes.object
+    muiTheme: React.PropTypes.object,
 };
 
 export default SearchCard;
