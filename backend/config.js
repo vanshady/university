f79b7c4df4c96a415070f52e3c697ae40cd258c3
Minwei Xu <faceswilliam@gmail.com>
2016-12-08 10:52:42 -0500

    refactor

--- backend/config.js
+++ backend/config.js
@@ -3,17 +3,4 @@ module.exports = {
   database: 'UNIVERSITY',
   username: 'root',
   password: null,
-  options: {
-    host: 'localhost',
-    dialect: 'mysql',
-    define: {
-      timestamps: false,
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000,
-    },
-    logging: false,
-  },
 };
