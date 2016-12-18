eed42f61832843fdd8667a35d9cc4bfa42e1d87f
Minwei Xu <faceswilliam@gmail.com>
2016-12-17 21:02:11 -0500

    allow mixed operator

--- frontend/js/application/components/satcard/SATCard.jsx
+++ frontend/js/application/components/satcard/SATCard.jsx
@@ -5,64 +5,177 @@ import * as ReactFauxDOM from 'react-faux-dom';
 import Dimensions from 'react-dimensions';
 import mockdata from './mockdata';
 
+// const request = require('superagent');
+
 class SATCard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             expanded: false,
+            sat: [],
         };
+
+        this.renderSVG = this.renderSVG.bind(this);
+
+        // const self = this;
+        // request
+        //     .get(self.props.url + '/all_sat/')
+        //     .end((err, res) => {
+        //         if (res) {
+        //             self.setState({
+        //                 universities: JSON.parse(res.text),
+        //             });
+        //         }
+        //     });
     }
 
     renderSVG() {
         const div = ReactFauxDOM.createElement('div');
+
         if (this.state.expanded) {
+            // initialize the dimensions
+            const padding = 20;
             const margin = { top: 20, right: 20, bottom: 30, left: 50 };
             const svg = d3.select(div).append('svg')
                 .attr('width', this.props.containerWidth - margin.left - margin.right)
                 .attr('height', 500);
             const width = +svg.attr('width') - margin.left - margin.right;
             const height = +svg.attr('height') - margin.top - margin.bottom - 10;
-            const g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-            const parseTime = d3.timeParse('%d-%b-%y');
+            const midline = (height - padding) / 2;
 
-            const x = d3.scaleTime()
-                .rangeRound([0, width]);
+            // initialize the x scale
+            const xScale = d3.scaleLinear()
+                        .range([padding, width - padding]);
 
-            const y = d3.scaleLinear()
-                .rangeRound([height, 0]);
+            // initialize the x axis
+            const xAxis = d3.axisBottom()
+                        .scale(xScale);
 
-            const line = d3.line()
-                .x(d => x(d.date))
-                .y(d => y(d.close));
+            function randomJitter() {
+                const seed = Math.round(Math.random() * 1) === 0 ? -5 : 5;
 
-            const data = mockdata.map(d => ({ date: parseTime(d.date),
-                close: parseFloat(d.close) }));
+                return midline + Math.floor((Math.random() * seed) + 1);
+            }
 
-            x.domain(d3.extent(data, d => d.date));
-            y.domain(d3.extent(data, d => d.close));
+            const csv = mockdata.map(d => ({ date: d.date, value: parseFloat(d.value) }));
+            let data = csv.map(d => d.value);
 
-            g.append('g')
-                .attr('class', 'axis axis--x')
-                .attr('transform', 'translate(0,' + height + ')')
-                .call(d3.axisBottom(x));
-            
-            g.append('text')
-                .attr('fill', '#000')
-                .attr('x', width / 2)
-                .attr('y', height + 20)
-                .attr('dy', '0.71em')
-                .style('text-anchor', 'middle')
-                .text('SAT Median');
+            data = data.sort(d3.ascending);
 
-            g.append('g')
-                .attr('class', 'axis axis--y')
-                .call(d3.axisLeft(y));
+            // calculate the boxplot statistics
+            // const minVal = data[0];
+            const q1Val = d3.quantile(data, 0.25);
+            const medianVal = d3.quantile(data, 0.5);
+            const q3Val = d3.quantile(data, 0.75);
+            const iqr = q3Val - q1Val;
+            const maxVal = data[data.length - 1];
+            const outliers = [];
+            let lowerWhisker = Infinity;
+            let upperWhisker = -Infinity;
 
-            g.append('path')
-                .datum(data)
-                .attr('class', 'line')
-                .attr('d', line);
+            // lowerWhisker = d3.max([minVal, q1Val - iqr])
+            // upperWhisker = d3.min([maxVal, q3Val + iqr]);
+
+            let index = 0;
+
+            // search for the lower whisker, the mininmum value within q1Val - 1.5*iqr
+            while (index < data.length && lowerWhisker === Infinity) {
+                if (data[index] >= (q1Val - 1.5 * iqr)) {
+                    lowerWhisker = data[index];
+                } else {
+                    outliers.push(data[index]);
+                }
+
+                index += 1;
+            }
+
+            index = data.length - 1; // reset index to end of array
+
+            // search for the upper whisker, the maximum value within q1Val + 1.5*iqr
+            while (index >= 0 && upperWhisker === -Infinity) {
+                if (data[index] <= (q3Val + 1.5 * iqr)) {
+                    upperWhisker = data[index];
+                } else {
+                    outliers.push(data[index]);
+                }
+
+                index -= 1;
+            }
+
+            // map the domain to the x scale +10%
+            xScale.domain([0, maxVal * 1.10]);
+
+            // append the axis
+            svg.append('g')
+                .attr('class', 'axis')
+                .attr('transform', 'translate(0, ' + (height - padding) + ')')
+                .call(xAxis);
+
+            // draw verical line for lowerWhisker
+            svg.append('line')
+                .attr('class', 'whisker')
+                .attr('x1', xScale(lowerWhisker))
+                .attr('x2', xScale(lowerWhisker))
+                .attr('stroke', 'black')
+                .attr('y1', midline - 10)
+                .attr('y2', midline + 10);
+
+            // draw vertical line for upperWhisker
+            svg.append('line')
+                .attr('class', 'whisker')
+                .attr('x1', xScale(upperWhisker))
+                .attr('x2', xScale(upperWhisker))
+                .attr('stroke', 'black')
+                .attr('y1', midline - 10)
+                .attr('y2', midline + 10);
+
+            // draw horizontal line from lowerWhisker to upperWhisker
+            svg.append('line')
+                .attr('class', 'whisker')
+                .attr('x1', xScale(lowerWhisker))
+                .attr('x2', xScale(upperWhisker))
+                .attr('stroke', 'black')
+                .attr('y1', midline)
+                .attr('y2', midline);
+
+            // draw rect for iqr
+            svg.append('rect')
+                .attr('class', 'box')
+                .attr('stroke', 'black')
+                .attr('fill', 'white')
+                .attr('x', xScale(q1Val))
+                .attr('y', padding + midline - 30)
+                .attr('width', xScale(iqr) - padding)
+                .attr('height', 20);
+
+            // draw vertical line at median
+            svg.append('line')
+                .attr('class', 'median')
+                .attr('stroke', 'black')
+                .attr('x1', xScale(medianVal))
+                .attr('x2', xScale(medianVal))
+                .attr('y1', midline - 10)
+                .attr('y2', midline + 10);
+
+            // draw data as points
+            svg.selectAll('circle')
+                .data(csv)
+                .enter()
+                .append('circle')
+                .attr('r', 2.5)
+                .attr('class', (d) => {
+                    if (d.value < lowerWhisker || d.value > upperWhisker) {
+                        return 'outlier';
+                    }
+
+                    return 'point';
+                })
+                .attr('cy', () => randomJitter())
+                .attr('cx', d => xScale(d.value))
+                .append('title')
+                .text(d => `Date: ${d.date}; value: ${d.value}`);
+
             return div.toReact();
         }
 
@@ -85,6 +198,10 @@ class SATCard extends React.Component {
 
 SATCard.childContextTypes = {
     muiTheme: React.PropTypes.object,
+};
+
+SATCard.PropTypes = {
+    containerWidth: React.PropTypes.number,
 };
 
 export default Dimensions()(SATCard);
